bb3d6c8c79291301fde234d52c0b6100b60fa3b6
darren wei <darrenwei666@163.com>
2020-06-04 21:20:09 +0800

    Update fp.js

--- code/fp.js
+++ code/fp.js
@@ -282,10 +282,17 @@ console.log(salaryLevel2(3000)); */
 */
 
 //纯函数
-let array = [1,2,3,4,5];
+/* let array = [1,2,3,4,5];
 console.log(array.slice(0,3));
-console.log(array.slice(0,3));
+console.log(array.slice(0,3)); */
 
 //不纯的函数
-console.log(array.splice(0,3));
-console.log(array.splice(0,3));
+/* console.log(array.splice(0,3));
+console.log(array.splice(0,3)); */
+
+
+
+
+/* 
+    
+*/
